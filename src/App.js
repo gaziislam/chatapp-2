ee1a8039aec0c0c948a18efad4086bfac614925d
Gazi Thouhidul Islam <g.t.islamm@gmail.com>
2022-10-07 00:53:29 -0400

    Dark mode done
    p 36
    
    34:20

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import Registration from "./pages/Registration"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import { BsToggleOff, BsToggleOn } from "react-icons/bs"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
 
 function App() {
-  let [dl, setDl] = useState(true)
+  const auth = getAuth()
+  let [dl, setDl] = useState(false)
+  let [show, setShow] = useState(false)
 
   let handleDarkLight = () => {
     setDl(!dl)
   }
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setShow(true)
+      } else {
+        setShow(false)
+      }
+    })
+  }, [])
+
   return (
     <>
       <div className={dl ? "dark" : "light"}>
-        <div className="dlmode" onClick={handleDarkLight}>
-          {dl ? (
-            <span className="off">
-              <BsToggleOff />
-            </span>
-          ) : (
-            <span className="on">
-              <BsToggleOn />
-            </span>
-          )}
-        </div>
+        {show && (
+          <div className="dlmode" onClick={handleDarkLight}>
+            {dl ? (
+              <>
+                <span className="off">
+                  <BsToggleOff />
+                </span>
+                <span className="text">Switch to light</span>
+              </>
+            ) : (
+              <>
+                <span className="on">
+                  <BsToggleOn />
+                </span>
+                <span className="text">Switch to dark</span>
+              </>
+            )}
+          </div>
+        )}
 
         <Routes>
           <Route path="/" element={<Registration />}></Route>
